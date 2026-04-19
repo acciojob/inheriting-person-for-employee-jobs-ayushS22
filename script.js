1be8dc449a9a825cc12a589ac9efd420c601b4d0
Ayush Sahu <115640976+ayushS22@users.noreply.github.com>
2026-04-19 12:03:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,28 +3,27 @@ function Person(name, age) {
   this.age = age;
 }
 
-// greet method
+// FIXED greet format
 Person.prototype.greet = function () {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
 
 function Employee(name, age, jobTitle) {
-  // call Person constructor
   Person.call(this, name, age);
   this.jobTitle = jobTitle;
 }
 
-// inherit from Person
+// inheritance
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
-// jobGreet method
+// jobGreet (this one is already correct format)
 Employee.prototype.jobGreet = function () {
   console.log(
     `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
   );
 };
 
-// Do not change code below this line
+// Do not change below
 window.Person = Person;
 window.Employee = Employee;
